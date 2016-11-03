d7f811ba73fb6573b8614f41e810fa6b9133e316
taomylife521 <514935323@qq.com>
2016-11-03 15:53:33 +0800

    新增aa方法

--- ND.Component.RabbitMQ/Class1.cs
+++ ND.Component.RabbitMQ/Class1.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace ND.Component.RabbitMQ
 {
     //wuyong
+    //test
     public class Class1
     {
+        public void aa()
+        {
+
+        }
     }
 }
