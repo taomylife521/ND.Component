e8517cdda5ab924649315eb61bb93497a5c0bf65
taomylife521 <514935323@qq.com>
2016-11-10 09:45:16 +0800

    添加test方法

--- ND.Component.Test/Program.cs
+++ ND.Component.Test/Program.cs
@@ -1,6 +1,10 @@
-﻿using ND.Component.Log;
+﻿using ND.Component.Caching;
+using ND.Component.ComponentBuilder;
+using ND.Component.Config;
+using ND.Component.Log;
 using ND.Component.Log.Log4Net;
 using ND.Component.Log.NLog;
+using ND.Component.MongoDB;
 using ND.Component.Log.NLogComponent;
 using ND.Component.MessageBus;
 using ND.Component.Redis.MessageBus;
@@ -10,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 
 
@@ -23,11 +28,55 @@ namespace ND.Component.Test
           
           
             #region ND.Component.Log
-            NDLoggerFactoryManger.AddFactory(LogCategory.NLog, new NLogLoggerFactory());
+            //NDLoggerFactoryManger.AddFactory(LogCategory.NLog, new NLogLoggerFactory());
            // NDLoggerFactoryManger.AddFactory(LogCategory.Custom, new CustomFileLoggerFactory());
            // NDLoggerFactoryManger.AddFactory(LogCategory.Log4Net, new Log4NetLoggerFactory());
-            NDLoggerFactoryManger.Instance.Error("i am error2", 1);
+           // NDLoggerFactoryManger.Instance.Error("i am error2", 1);
            
+            #endregion
+
+            #region MyRegion
+            IComponentBuilder build = new DefaultComponentBuilder();
+            build.Build()
+                .UseLog4Net()
+                .UseNLog()
+                .UseMongoDBCache();
+            Stopwatch wt = new Stopwatch();
+            wt.Start();
+            int sum = 0;
+            int writeSucCount = 0;
+            int writeFailedCount = 0;
+            int rSucCount = 0;
+            int rFailedCount = 0;
+            for (int i = 0; i < 10; i++)//1千万
+            {
+
+                bool r = CacheManger.Instance.SetValue(i.ToString(), "b" + i.ToString(), Cachelimit.ByExpireDate,DateTime.Now.AddMinutes(1));
+                if (r)
+                {
+                    writeSucCount++;
+                }
+                else
+                {
+                    writeFailedCount++;
+                }
+                string bb = "";
+                CacheManger.Instance.TryGetValue(i.ToString(), out bb);
+                if(string.IsNullOrEmpty(bb))
+                {
+                    rFailedCount++;
+                }
+                else
+                {
+                    rSucCount++;
+                }
+                sum++;
+                Console.WriteLine("key:"+i.ToString()+",value:"+bb);
+            }
+            wt.Stop();
+            Console.WriteLine("耗时:"+wt.ElapsedMilliseconds/1000);
+            Console.WriteLine("写入成功率:"+writeSucCount/sum+",写入失败率："+writeFailedCount/sum);
+            Console.WriteLine("读取成功率:" + rSucCount / sum + ",读取失败率：" + rFailedCount / sum);
             #endregion
 
            
